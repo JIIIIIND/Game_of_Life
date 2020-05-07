1b62c8ce5869a05ca6becc21991b93136cd8c1ba
JIND <jinwoo700@gmail.com>
2020-05-07 14:25:24 +0900

    separate btn_event function

--- linux_version/srcs/btn_event.c
+++ linux_version/srcs/btn_event.c
@@ -6,20 +6,27 @@
 /*   By: jinwkim <jinwkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/04 19:07:21 by jinwkim           #+#    #+#             */
-/*   Updated: 2020/05/07 13:34:40 by jinwkim          ###   ########.fr       */
+/*   Updated: 2020/05/07 14:24:21 by jinwkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game_of_life.h"
 #include "get_next_line.h"
 #include "serialize.h"
-#include <stdio.h>
 
-int		btn_check(int button, int x, int y, t_map *map)
+void	save_load_event(t_map *map, void (*f)())
 {
 	char	*path;
 
 	path = 0;
+	ft_putstr_fd("input file name\n", 1);
+	get_next_line(0, &path);
+	(*f)(map, path);
+	free(path);
+}
+
+int		btn_check(int button, int x, int y, t_map *map)
+{
 	if ((button == 1) && (x > 529) && (x < 592))
 	{
 		if ((y > 188) && (y < 251))
@@ -35,19 +42,9 @@ int		btn_check(int button, int x, int y, t_map *map)
 				map->speed += 10;
 		}
 		else if ((y > 428) && (y < 450))
-		{
-			printf("input file name\n");
-			get_next_line(0, &path);
-			save_file(map, path);
-			free(path);
-		}
+			save_load_event(map, &save_file);
 		else if ((y > 468) && (y < 490))
-		{
-			printf("load\n");
-			get_next_line(0, &path);
-			load_file(map, path);
-			free(path);
-		}
+			save_load_event(map, &load_file);
 	}
 	return (0);
 }
